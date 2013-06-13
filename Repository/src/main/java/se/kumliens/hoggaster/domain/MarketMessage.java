a78570b38f615fa5fdcd906b035e424aa326b9b6
kumlien <svante.kumlien@gmail.com>
2013-06-13 17:03:59 +0200

    asdf

--- Repository/src/main/java/se/kumliens/hoggaster/domain/MarketMessage.java
+++ Repository/src/main/java/se/kumliens/hoggaster/domain/MarketMessage.java
@@ -1,6 +1,9 @@
 package se.kumliens.hoggaster.domain;
 
+import java.util.Date;
+
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
@@ -8,4 +11,43 @@ public class MarketMessage {
 	
 	@Id
 	private String id;
+	
+	@Indexed
+	private String instrument;
+	
+	private String type;
+	
+	private Date date;
+
+	public String getInstrument() {
+		return instrument;
+	}
+
+	public void setInstrument(String instrument) {
+		this.instrument = instrument;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 }
--- Repository/src/main/java/se/kumliens/hoggaster/repo/mongodb/MongoMarketMessageDAO.java
+++ Repository/src/main/java/se/kumliens/hoggaster/repo/mongodb/MongoMarketMessageDAO.java
@@ -1,13 +1,51 @@
 package se.kumliens.hoggaster.repo.mongodb;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
+import se.kumliens.hoggaster.domain.MarketMessage;
 import se.kumliens.hoggaster.repo.MarketMessageDAO;
 
 @Repository
 public class MongoMarketMessageDAO implements MarketMessageDAO {
 	
 	@Autowired MongoOperations mongoDb;
+
+	@Override
+	public void store(MarketMessage message) {
+		mongoDb.save(message);
+	}
+
+
+	@Override
+	public void delete(Set<MarketMessage> messages) {
+		Set<String> ids = new HashSet<String>();
+		for(MarketMessage message : messages) {
+			ids.add(message.getId());
+		}
+		Query query = new Query(Criteria.where("_id").in(ids));
+		mongoDb.remove(query, MarketMessage.class);
+		
+	}
+
+	@Override
+	public List<MarketMessage> findByInstrument(String instrument) {
+		Query query = new Query(Criteria.where("instrument").is(instrument));
+		return mongoDb.find(query, MarketMessage.class);
+	}
+
+
+	@Override
+	public void deleteOlderThan(Date date) {
+		Query query = new Query(Criteria.where("date").lt(date));
+		mongoDb.remove(query, MarketMessage.class);
+	}
 }
